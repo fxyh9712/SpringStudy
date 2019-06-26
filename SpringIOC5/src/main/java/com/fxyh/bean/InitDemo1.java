23c71361dee0a990d5b88dc8b938f566fadf1f52
fxyh97 <1351534609@qq.com>
2019-06-26 13:30:19 +0800

    bean初始化流程

--- SpringIOC5/src/main/java/com/fxyh/bean/InitDemo1.java
+++ SpringIOC5/src/main/java/com/fxyh/bean/InitDemo1.java
@@ -1,35 +1,37 @@
 package com.fxyh.bean;
 
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.BeanFactoryAware;
-import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.*;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 import java.io.Serializable;
 
 /**
  * @ClassName: InitDemo1
- * @description: TODO
+ * @description: bean初始化流程
  * @author: fengzhaoquan
  * @create: 2019-06-20 14:59
  * @Version 1.0
  **/
-public class InitDemo1 implements Serializable, BeanNameAware, BeanFactoryAware {
+
+public class InitDemo1 implements Serializable, BeanNameAware, ApplicationContextAware, BeanFactoryAware, BeanPostProcessor, InitializingBean, DisposableBean {
     private static final long serialVersionUID = -2167328278489217537L;
 
     public InitDemo1() {
         System.out.println("InitDemo1构造方法调用！");
     }
 
-    public void init(){
+    public void myInit() {
         System.out.println("自定义init方法调用！");
     }
 
-    public void destroy(){
+    public void myDestroy() {
         System.out.println("自定义destroy方法调用！");
     }
 
-    public void service(){
+    public void service() {
         System.out.println("service方法调用！");
     }
 
@@ -41,5 +43,32 @@ public class InitDemo1 implements Serializable, BeanNameAware, BeanFactoryAware 
     @Override
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
         System.out.println("setBeanFactory:" + beanFactory + "调用！");
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessBeforeInitialization: beanName=" + beanName);
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessAfterInitialization: beanName=" + beanName);
+        return bean;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("setApplicationContext:" + applicationContext);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("afterPropertiesSet调用");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("destory方法调用！");
     }
 }
